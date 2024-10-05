fae4c7e770c09116d039dd5f54e89d6e918f3beb
Ayormitan <kolawoleabaz97@gmail.com>
2024-10-05 10:43:41 -0700

    Function resolve promise

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,6 +1,7 @@
-import createUser from './utils.js';
-
 export default function signUpUser(firstName, lastName) {
   // Create and resolve a promise with firstname and lastname object
-  return Promise.resolve({ firstName, lastName });
+  return Promise.resolve({
+    firstName,
+    lastName,
+  });
 }
